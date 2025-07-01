e1f4ab3a0cf9f44d69f87a1dbd95412796f226ce
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-07-01 11:39:25 +0900

    [feat #18] 사원 목록 조회 수정
    - 상세 조회 페이지로 라우팅

--- src/features/employee/api.js
+++ src/features/employee/api.js
@@ -4,3 +4,8 @@ export const getEmployees = async (params) => {
     const resp = await api.get("/employees", { params });
     return resp.data.data;
 }
+
+export const getEmployeeDetails = async (empId) => {
+    const resp = await api.get($`/employees/{empId}`);
+    return resp.data.data;
+}
--- src/features/employee/views/EmployeeListView.vue
+++ src/features/employee/views/EmployeeListView.vue
@@ -3,21 +3,17 @@ import {computed, onMounted, ref, watch} from "vue";
 import Pagination from "@/components/common/Pagination.vue";
 import Filter from "@/components/common/Filter.vue";
 import BaseTable from "@/components/common/BaseTable.vue";
-import SideModal from "@/components/common/SideModal.vue";
 import HeaderWithTabs from "@/components/common/HeaderWithTabs.vue";
 import {getPositions} from "@/features/works/api.js";
 import {getEmployees} from "@/features/employee/api.js";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
 
 const currentPage = ref(1);
 const pagination = ref({currentPage: 1, totalPage: 1});
 const filterValues = ref({});
-// const appliedFilterValues = ref({});
 const employees = ref([]);
-
-const showModal = ref(false);
-const modalWorkId = ref(null);
-const workDetails = ref(null);
-const loadingDetails = ref(false);
 
 const positionOptions = ref([]);
 
@@ -34,19 +30,24 @@ const deptOptions = ref([
 ]);
 
 const columns = [
-    {key: 'empNo', label: '사번'},
-    {key: 'name', label: '이름'},
-    {key: 'deptName', label: '부서'},
-    {key: 'positionName', label: '직위'},
-    {key: 'userRoles', label: '권한', format: val => val.map(x => roleMap[x]).join(", ")},
-    {key: 'joinDate', label: '입사일', format: val => val},
-    {key: 'status', label: '재직 상태', format: val => {
+  {key: 'empNo', label: '사번'},
+  {key: 'name', label: '이름'},
+  {key: 'deptName', label: '부서'},
+  {key: 'positionName', label: '직위'},
+  {key: 'userRoles', label: '권한', format: val => val.map(x => roleMap[x]).join(", ")},
+  {key: 'joinDate', label: '입사일', format: val => val},
+  {key: 'status', label: '재직 상태', format: val => {
       switch (val) {
-        case 'EMPLOYED': return '재직';
-        case 'ON_LEAVE': return '휴직';
-        case 'RESIGNED': return '퇴사';
+        case 'EMPLOYED':
+          return '재직';
+        case 'ON_LEAVE':
+          return '휴직';
+        case 'RESIGNED':
+          return '퇴사';
       }
-      }}
+    }
+  },
+  {key: 'action', label: '상세'},
 ];
 
 // 필터
@@ -55,8 +56,19 @@ const baseFilterOptions = computed(() => [
   {key: 'positionId', type: 'select', label: '직위', icon: 'fa-user-tie', options: positionOptions.value},
   {key: 'userRole', type: 'select', label: '권한', icon: 'fa-shield-alt', options: roleOptions.value},
   {key: 'joinDate', type: 'date-range', label: '입사일', icon: 'fa-calendar-day'},
-  {key: 'sortBy', type: 'select', label: '정렬 기준', options: [{label: '사번', value: null}, {label:'입사일', value:'JOIN_DATE'}, {label:'직위', value:'POSITION'}]},
-  {key: 'order', type: 'select', label: '정렬', icon:'fa-filter', options: [{label:'오름차순', value:'ASC'}, {label: '내림차순', value:'DESC'}]}
+  {
+    key: 'sortBy',
+    type: 'select',
+    label: '정렬 기준',
+    options: [{label: '사번', value: null}, {label: '입사일', value: 'JOIN_DATE'}, {label: '직위', value: 'POSITION'}]
+  },
+  {
+    key: 'order',
+    type: 'select',
+    label: '정렬',
+    icon: 'fa-filter',
+    options: [{label: '오름차순', value: 'ASC'}, {label: '내림차순', value: 'DESC'}]
+  }
 ]);
 
 const filterOptions = computed(() => [...baseFilterOptions.value]);
@@ -112,7 +124,7 @@ const roleMap = {
 
 const roleOptions = ref({});
 roleOptions.value = [
-  { label: '전체', value: null },
+  {label: '전체', value: null},
   ...Object.keys(roleMap).map(key => ({
     label: roleMap[key],
     value: key
@@ -128,96 +140,31 @@ onMounted(async () => {
 
 watch(currentPage, () => fetchSummary(filterValues.value));
 
-const openDetailsModal = async (work) => {
-  // const workId = work.workId;
-  // modalWorkId.value = workId;
-  // showModal.value = true;
-  // loadingDetails.value = true;
-  //
-  // try {
-  //   workDetails.value = await getWorkDetails(workId);
-  // } catch (error) {
-  //   workDetails.value = null;
-  // } finally {
-  //   loadingDetails.value = false;
-  // }
 
+const goToDetailsPage = (emp) => {
+  const empId = emp.empId;
+  router.push(`/employees/${empId}`);
 };
-//
-// const closeModal = () => {
-//   showModal.value = false;
-//   modalWorkId.value = null;
-//   workDetails.value = null;
-// }
-//
-// const formatPushedAt = (val) => val ? val.replace('T', ' ') : '-';
-// const formatDuration = (minutes) => {
-//   const h = Math.floor(minutes / 60);
-//   const m = minutes % 60;
-//   return (h >= 1 ? `${h}시간 ` : '') + `${m}분`;
-// }
-//
-// const modalSections = computed(() => {
-//   if (!workDetails.value) return [];
-//
-//   // 예시: workDetails 값에 맞게 키와 라벨, 값 매핑
-//   // 필드는 key, label, value 순으로 필드 렌더러가 읽음
-//   return [
-//     {
-//       title: '기록 정보',
-//       icon: 'fa-info-circle',
-//       layout: 'one-column',
-//       fields: [
-//         {
-//           key: 'startPushedAt',
-//           label: '출근 등록 일시',
-//           value: formatPushedAt(workDetails.value.startPushedAt)
-//         },
-//         {
-//           key: 'endPushedAt',
-//           label: '퇴근 등록 일시',
-//           value: formatPushedAt(workDetails.value.endPushedAt)
-//         },
-//         {
-//           key: 'breakTime',
-//           label: '휴게 시간',
-//           value: formatDuration(workDetails.value.breakTime)
-//         },
-//         { key: 'isNormalWork', label: '정상 근무 여부', value: workDetails.value.isNormalWork}
-//       ],
-//     },
-//   ];
-// });
 </script>
 
 <template>
   <main>
     <HeaderWithTabs :headerItems="[
         { label: '사원 목록 조회', to: '/employees', active: true },
-    ]" :showTabs="false"/>
+    ]"
+                    :submitButtons="[{ label: '등록', icon: 'fa-user-plus', event: 'register', variant: 'blue' },
+    {label: 'CSV', icon: 'fa-upload', event: 'csv', variant: 'green'}]"
+                    :showTabs="false"/>
     <Filter :filters="filterOptions" v-model="filterValues" @search="handleSearch"/>
 
+
     <!-- Table Section -->
-    <BaseTable :columns="columns" :rows="employees" @click-detail="openDetailsModal"/>
+    <BaseTable :columns="columns" :rows="employees" @click-detail="goToDetailsPage"/>
 
     <!-- Pagination -->
     <Pagination
         v-if="pagination.totalPage"
         v-model="currentPage"
         :total-pages="pagination.totalPage"/>
-
-    <!-- 출퇴근 상세 모달 -->
-<!--    <SideModal-->
-<!--        v-if="showModal"-->
-<!--        :visible="showModal"-->
-<!--        :title="'상세 출퇴근 정보'"-->
-<!--        icon="fa-clock"-->
-<!--        :sections="modalSections"-->
-<!--        :readonly="true"-->
-<!--        @update:visible="showModal = $event"-->
-<!--        @close="closeModal"-->
-<!--        :showSubmit="false"-->
-<!--    />-->
-
   </main>
 </template>
