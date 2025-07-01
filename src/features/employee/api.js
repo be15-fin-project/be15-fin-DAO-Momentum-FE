5edad6cfe2dd55a9d68ff43387aa070d36c26bb7
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-07-01 14:43:41 +0900

    [feat #18] 사원 등록 기능 구현
    - 사원 등록 구현 (권한 설정 부분 수정 필요)

--- src/features/employee/api.js
+++ src/features/employee/api.js
@@ -9,3 +9,8 @@ export const getEmployeeDetails = async (empId) => {
     const resp = await api.get($`/employees/{empId}`);
     return resp.data.data;
 }
+
+export const createEmployee = async (req) => {
+    const resp = await api.post('/employees', req);
+    return resp.data.data;
+}
--- src/features/employee/views/EmployeeListView.vue
+++ src/features/employee/views/EmployeeListView.vue
@@ -4,8 +4,9 @@ import Pagination from "@/components/common/Pagination.vue";
 import Filter from "@/components/common/Filter.vue";
 import BaseTable from "@/components/common/BaseTable.vue";
 import HeaderWithTabs from "@/components/common/HeaderWithTabs.vue";
+import SideModal from "@/components/common/SideModal.vue";
 import {getPositions} from "@/features/works/api.js";
-import {getEmployees} from "@/features/employee/api.js";
+import {createEmployee, getEmployees} from "@/features/employee/api.js";
 import {useRouter} from "vue-router";
 
 const router = useRouter();
@@ -15,9 +16,11 @@ const pagination = ref({currentPage: 1, totalPage: 1});
 const filterValues = ref({});
 const employees = ref([]);
 
-const positionOptions = ref([]);
+const positionFilterOptions = ref([]);
 
-const deptOptions = ref([
+const showModal = ref(false);
+
+const deptFilterOptions = ref([
   {label: '전체', value: null},
   {label: '테크놀로지(주)', value: 1},
   {label: '인사팀', value: 10},
@@ -32,11 +35,12 @@ const deptOptions = ref([
 const columns = [
   {key: 'empNo', label: '사번'},
   {key: 'name', label: '이름'},
-  {key: 'deptName', label: '부서'},
+  {key: 'deptName', label: '부서', format: val => val ?? '-'},
   {key: 'positionName', label: '직위'},
   {key: 'userRoles', label: '권한', format: val => val.map(x => roleMap[x]).join(", ")},
   {key: 'joinDate', label: '입사일', format: val => val},
-  {key: 'status', label: '재직 상태', format: val => {
+  {
+    key: 'status', label: '재직 상태', format: val => {
       switch (val) {
         case 'EMPLOYED':
           return '재직';
@@ -52,8 +56,8 @@ const columns = [
 
 // 필터
 const baseFilterOptions = computed(() => [
-  {key: 'deptId', type: 'select', label: '부서', icon: 'fa-building', options: deptOptions.value},
-  {key: 'positionId', type: 'select', label: '직위', icon: 'fa-user-tie', options: positionOptions.value},
+  {key: 'deptId', type: 'select', label: '부서', icon: 'fa-building', options: deptFilterOptions.value},
+  {key: 'positionId', type: 'select', label: '직위', icon: 'fa-user-tie', options: positionFilterOptions.value},
   {key: 'userRole', type: 'select', label: '권한', icon: 'fa-shield-alt', options: roleOptions.value},
   {key: 'joinDate', type: 'date-range', label: '입사일', icon: 'fa-calendar-day'},
   {
@@ -133,17 +137,122 @@ roleOptions.value = [
 
 onMounted(async () => {
   const positions = await getPositions();
-  positionOptions.value = [{label: '전체', value: null}, ...positions.map(p => ({label: p.name, value: p.positionId}))];
+  positionFilterOptions.value = [{label: '전체', value: null}, ...positions.map(p => ({label: p.name, value: p.positionId}))];
+  positionOptions.value = [{label: '선택', value: null}, ...positions.map(p => ({label: p.name, value: p.positionId}))];
   handleSearch();
   filterValues.value = {};
 });
 
 watch(currentPage, () => fetchSummary(filterValues.value));
 
-
 const goToDetailsPage = (emp) => {
   const empId = emp.empId;
   router.push(`/employees/${empId}`);
+};
+
+const deptOptions = ref([
+  {label: '선택', value: null},
+  {label: '테크놀로지(주)', value: 1},
+  {label: '인사팀', value: 10},
+  {label: '재무팀', value: 11},
+  {label: '프론트엔드팀', value: 12},
+  {label: '백엔드팀', value: 13},
+  {label: '데이터팀', value: 14},
+  {label: '영업팀', value: 15},
+  {label: '디지털마케팅팀', value: 16},
+]);
+
+const positionOptions = ref({});
+
+const initialFormData = {
+  name: '',
+  birthDate: null,
+  email: '',
+  contact: '',
+  address: '',
+  deptId: null,
+  positionId: null,
+  employeeRoles: [],
+  status: 'EMPLOYED',
+  joinDate: null,
+  remainingDayoffHours: 0,
+  remainingRefreshDays: 0,
+  gender: null,
+};
+
+const formData = ref({...initialFormData});
+
+const openCreateModal = () => {
+  formData.value = {...initialFormData};
+  showModal.value = true;
+}
+
+const closeModal = () => {
+  showModal.value = false;
+}
+
+const modalSections = computed(() => [
+  {
+    title: '기본 정보',
+    icon: 'fa-user',
+    layout: 'two-column',
+    fields: [
+      {key: 'name', label: '이름', type: 'input', editable: true, required: true, placeholder: '홍길동'},
+      {
+        key: 'gender', label: '성별', type: 'select', editable: true, required: true, options: [
+          {label: '선택', value: null},
+          {label: '남성', value: 'M'},
+          {label: '여성', value: 'F'}
+        ]
+      },
+      {key: 'birthDate', label: '생년월일', type: 'date', editable: true, required: true},
+      {key: 'email', label: '이메일', type: 'input', editable: true, required: true, placeholder: 'gildong@example.com'},
+      {key: 'contact', label: '연락처', type: 'input', editable: true, required: true, placeholder: '010-0000-0000'},
+      {key: 'address', label: '주소', type: 'input', editable: true, required: true, placeholder: '도로명 주소를 입력하세요.'},
+    ]
+  },
+  {
+    title: '근무 정보',
+    icon: 'fa-briefcase',
+    layout: 'two-column',
+    fields: [
+      {key: 'deptId', label: '부서', type: 'select', editable: true, options: deptOptions.value || []},
+      {key: 'positionId', label: '직위', type: 'select', editable: true, required: true, options: positionOptions.value || []},
+      {
+        key: 'status',
+        label: '재직 상태',
+        type: 'select',
+        editable: true,
+        required: true,
+        options: [
+          {label: '재직', value: 'EMPLOYED'},
+          {label: '휴직', value: 'ON_LEAVE'},
+          {label: '퇴사', value: 'RESIGNED'}
+        ]
+      },
+      {key: 'joinDate', label: '입사일', type: 'date', editable: true, required: true},
+      {key: 'remainingDayoffHours', label: '부여 연차 시간 (예: 15일 -> 120)', type: 'number', editable: true, required: true, placeholder: '120'},
+      {key: 'remainingRefreshDays', label: '부여 리프레시 휴가 일수', type: 'number', editable: true, required: true},
+      /* TODO: 멀티 셀렉트 형태로 수정 필요 (현재는 선택값 없는 경우만 등록 가능) */
+      {
+        key: 'employeeRoles',
+        label: '권한',
+        type: 'select',
+        editable: true,
+        options: Object.keys(roleMap).filter(key => key !== 'EMPLOYEE').map(key => ({label: roleMap[key], value: key}))
+      },
+    ]
+  },
+]);
+
+const handleRegisterSubmit = async (req) => {
+  try {
+    const resp = await createEmployee(req);
+    closeModal();
+    handleSearch(); // 목록 새로고침
+  } catch (e) {
+    console.error('등록 실패:', e);
+  }
 };
 </script>
 
@@ -152,9 +261,11 @@ const goToDetailsPage = (emp) => {
     <HeaderWithTabs :headerItems="[
         { label: '사원 목록 조회', to: '/employees', active: true },
     ]"
-                    :submitButtons="[{ label: '등록', icon: 'fa-user-plus', event: 'register', variant: 'blue' },
-    {label: 'CSV', icon: 'fa-upload', event: 'csv', variant: 'green'}]"
-                    :showTabs="false"/>
+                    :submitButtons="[{ label: '등록', icon: 'fa-user-plus', variant: 'blue'},
+    {label: 'CSV', icon: 'fa-upload', variant: 'green'}]"
+                    :showTabs="false"
+                    @click="openCreateModal"
+    />
     <Filter :filters="filterOptions" v-model="filterValues" @search="handleSearch"/>
 
 
@@ -166,5 +277,17 @@ const goToDetailsPage = (emp) => {
         v-if="pagination.totalPage"
         v-model="currentPage"
         :total-pages="pagination.totalPage"/>
+
+    <!-- 사원 등록 사이드 모달 -->
+    <SideModal
+        v-if="showModal"
+        :visible="showModal"
+        v-model:form="formData"
+        :title="'사원 등록'"
+        icon="fa-user-plus"
+        @close="closeModal"
+        :sections="modalSections"
+        @submit="handleRegisterSubmit(formData)"
+    />
   </main>
 </template>
