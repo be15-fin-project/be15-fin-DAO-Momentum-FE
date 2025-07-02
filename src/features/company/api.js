dde8f81123e6638518283886ba660423e859b9af
Taein5415 <kskk5050@naver.com>
2025-07-02 10:16:11 +0900

    [feat #84] 회사 조직도 조회 페이지 구현

--- src/features/company/api.js
+++ src/features/company/api.js
@@ -3,3 +3,12 @@ import api from '@/api/axios.js'
 export function fetchCompanyInfo() {
     return api.get('/company');
 }
+
+export async function fetchDepartments() {
+    const res = await api.get('/departments');
+    return res.data;
+}
+
+export function fetchDepartmentInfo(deptId){
+    return api.get(`/departments/${deptId}`)
+}
--- src/features/company/router.js
+++ src/features/company/router.js
@@ -1,4 +1,5 @@
 import CompanyInfoView from "@/features/company/views/CompanyInfoView.vue";
+import OrgChartView from "@/features/company/views/OrgChartView.vue";
 
 export const companyRoutes = [
     {
@@ -8,6 +9,11 @@ export const companyRoutes = [
                 path: 'company-info',
                 name: 'CompanyInfoView',
                 component: CompanyInfoView
+            },
+            {
+                path:'org-chart',
+                name: 'OrgChartView',
+                component: OrgChartView
             }
         ]
     }
