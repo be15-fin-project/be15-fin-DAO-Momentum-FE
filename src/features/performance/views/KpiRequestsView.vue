34cb6106e15f6fda0431e13f413516e4ceddc072
meowdule <meowdule73@gmail.com>
2025-07-18 14:43:14 +0900

    [refactor] header-title 조회 오류 수정

--- src/features/performance/views/KpiRequestsView.vue
+++ src/features/performance/views/KpiRequestsView.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <HeaderWithTabs
-        :headerItems="[{ label: 'KPI 요청 관리', href: '#', active: true }]"
+        :headerItems="[{ label: 'KPI 요청 관리', href: '/kpi/requests', active: true }]"
         :showTabs="false"
     />
 
