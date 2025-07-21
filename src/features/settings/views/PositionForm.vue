56e871d243658d5f6194ca51df7e7cd0c485e0c3
Taein5415 <kskk5050@naver.com>
2025-07-21 12:50:19 +0900

    [fix] 직위 단계 form 형식 수정

--- src/features/settings/views/PositionForm.vue
+++ src/features/settings/views/PositionForm.vue
@@ -37,7 +37,7 @@ const createModalSections = computed(() => [{
   fields:[{
     key: 'name', type: 'input', label: '직위명', editable: true, required: true
   }, {
-    key: 'level', type: 'input', label: '직위 단계', editable: true, required: true
+    key: 'level', type: 'number', label: '직위 단계', editable: true, required: true
   }]
 }]);
 
